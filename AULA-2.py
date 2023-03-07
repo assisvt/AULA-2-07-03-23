5b5395b5bb597da58f0a1579bd05b92832584aca
Vítor Assis <contatoassisvt@gmail.com>
2023-03-07 09:39:40 -0300

    Update AULA-2.py

--- AULA-2.py
+++ AULA-2.py
@@ -18,3 +18,21 @@ print("A multipicação é igual a", multiplicacao)
 soma_total = soma + subtração + multiplicacao
 print("A soma total dos valores lidos é", soma_total)
 --
+nota1 = 6.1
+nota2 = 5.1
+media = (nota1 + nota2)/2
+print(media)
+--
+input("Qual é o nome do aluno? ")
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+media = (nota1 + nota2)/2 # parênteses obrigatório
+print("A média do aluno é: ", media)
+--
+input("Qual é o nome do aluno? ")
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+nota3 = float(input("Digite a nota 3: "))
+media = (nota1 + nota2 + nota3)/3
+print("O aluno fez 3 avaliações e a média do aluno foi: ", media, ", mas relaxa, a nota máxima é 4.")
+
