54b33f83913e36db4974dd3bd9b9bce241794ec8
Vítor Assis <contatoassisvt@gmail.com>
2023-03-07 08:44:40 -0300

    Update AULA-2.py

--- AULA-2.py
+++ AULA-2.py
@@ -1,1 +1,20 @@
-
+valor1 = int(input("Digite o primeiro valor: "))  #entrada de dados
+valor2 = int(input("Digite o segundo valor: "))
+# efetuar o cáuculo atribuindo o resultado à variável soma
+soma = valor1 + valor2 # Processamento
+print("A soma é igual a ", soma) # Saída
+# print(f'O resultado da soma é  {soma}') #Mostra o resultado
+--
+valor1 = int(input("Digite o primeiro valor: "))  #entrada de dados
+valor2 = int(input("Digite o segundo valor: "))
+# efetuar o cáuculo atribuindo o resultado à variável soma
+soma = valor1 + valor2 # Processamento
+print("A soma é igual a ", soma) # Saída
+# print(f'O resultado da soma é  {soma}') #Mostra o resultado
+subtração = valor1 - valor2
+print("A subtração é igual a ", subtração)
+multiplicacao = valor1 * valor2
+print("A multipicação é igual a", multiplicacao)
+soma_total = soma + subtração + multiplicacao
+print("A soma total dos valores lidos é", soma_total)
+--
